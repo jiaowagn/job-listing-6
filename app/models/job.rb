0449db99e9af3fbf8495c5cd61f1a9cfdcb899bd
jiaowagn <wangjiaohere@gmail.com>
2017-09-17 17:07:57 +0800

    implement step4

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,15 @@
 class Job < ApplicationRecord
   validates_presence_of :title, :wage_lower_bound, :wage_upper_bound
   validates :wage_lower_bound, numericality: { greater_than: 0}
+
+  def hide!
+    self.is_hidden = true
+    self.save
+  end
+
+  def publish!
+    self.is_hidden = false
+    self.save 
+  end
+
 end
